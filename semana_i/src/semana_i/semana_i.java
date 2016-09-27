4ad14a7e3beab4d559eca20c80775af1d9e4de29
quiin <carlos.reyna21@gmail.com>
2016-09-27 18:34:58 -0500

    gitignore removed

--- semana_i/src/semana_i/semana_i.java
+++ semana_i/src/semana_i/semana_i.java
@@ -1,12 +1,22 @@
 package semana_i;
 
-import gameInterface.AIInterface;
+import java.util.Random;
+
 import structs.FrameData;
 import structs.GameData;
 import structs.Key;
+import gameInterface.AIInterface;
+
 
 public class semana_i implements AIInterface {
 
+	// Key class for return.
+	Key inputKey;
+	// is used for getting a random number.
+	Random rnd;
+	
+	boolean playerNumber;
+	
 	@Override
 	public void close() {
 		// TODO Auto-generated method stub
@@ -14,33 +24,42 @@ public class semana_i implements AIInterface {
 	}
 
 	@Override
-	public String getCharacter() {
-		// TODO Auto-generated method stub
-		return null;
+	public void getInformation(FrameData frameData) {
+		
 	}
 
 	@Override
-	public void getInformation(FrameData arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public int initialize(GameData arg0, boolean arg1) {
-		// TODO Auto-generated method stub
+	public int initialize(GameData gameData,boolean playerNumber) {
+		// initializes a Key instance.
+		inputKey = new Key();
+		// initializes a random instance.
+		rnd = new Random();
+		
 		return 0;
 	}
 
 	@Override
 	public Key input() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		// returns Key
+		return inputKey;
 	}
 
 	@Override
 	public void processing() {
-		// TODO Auto-generated method stub
+		// every key is set randomly.
+		inputKey.A = (rnd.nextInt(10) > 4) ? true : false;
+		inputKey.B = (rnd.nextInt(10) > 4) ? true : false;
+		inputKey.C = (rnd.nextInt(10) > 4) ? true : false;
+		inputKey.U = (rnd.nextInt(10) > 4) ? true : false;
+		inputKey.D = (rnd.nextInt(10) > 4) ? true : false;
+		inputKey.L = (rnd.nextInt(10) > 4) ? true : false;
+		inputKey.R = (rnd.nextInt(10) > 4) ? true : false;
+	}	
 
+	public String getCharacter(){
+		return CHARACTER_ZEN;
 	}
-
+	
 }
